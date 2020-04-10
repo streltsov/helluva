4322cbee3e3ce30dcd3e80b2d1c0047a79f48506
Herman Streltsov <mail@streltsov.dev>
2020-04-10 22:36:58 +0300

    Add description for prop function

--- docs/helluva.js
+++ docs/helluva.js
@@ -15,3 +15,31 @@ function head(xs) {
     return xs[0];
   }
 }
+
+function prop(string) {
+  if (typeof string != "string") {
+    throw new TypeError(
+      `Helluva.js: prop: Couldn't match expected type 'string', with actual type '${
+        string === null
+          ? "null"
+          : Array.isArray(string)
+          ? "array"
+          : typeof string
+      }':\n           ${JSON.stringify(string)}\n`
+    );
+  }
+
+  return function (obj) {
+    if (Array.isArray(obj) || typeof obj != "object" || obj === null) {
+      throw new TypeError(
+        `Helluva.js: prop: Couldn't match expected type 'object', with actual type '${
+          obj === null ? "null" : Array.isArray(obj) ? "array" : typeof obj
+        }':\n           ${JSON.stringify(obj)}\n`
+      );
+    }
+    if (!obj.hasOwnProperty(string)) {
+      throw `Object has not own property: ${string}`;
+    }
+    return obj[string];
+  };
+}
